883822cab511dbb1cbdaa8046b676c09aa4b1fbd
Charles <charles.m.long43@gmail.com>
2018-07-12 21:03:21 -0500

    angular route

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,18 @@ const path = require('path');
 /* GET home pages. */
 console.log(__rootpath)
 // pug test
-router.get('/pug-test', function(req, res, next) {
+router.get('/pug-test', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
 // react
-router.get('/react', function(req, res, next) {
+router.get('/react', function(req, res) {
   res.sendFile(path.resolve(__rootpath, 'react/build/index.html'));
 });
 
 // angular
-// router.get('/angular', function(req, res, next) {
-//   res.sendFile(path.resolve(__rootpath, 'xxx'));
-// });
+router.get('/angular', function(req, res) {
+  res.sendFile(path.resolve(__rootpath, 'angular/src/index.html'));
+});
 
 module.exports = router;
