a3fd7bb8a23d82fde243a51b25db9d614a2222c4
Charles <charles.m.long43@gmail.com>
2018-07-13 23:33:49 -0500

    Part-4 complete

--- react/src/Heroes/Heroes.js
+++ react/src/Heroes/Heroes.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Heroes.css';
-
 import { INITIAL_HEROES } from '../mock-heroes';
 
 export default class Heroes extends Component {
@@ -8,44 +7,50 @@ export default class Heroes extends Component {
     super();
     this.state = {
       selectedIdx: null,
-      heroes: INITIAL_HEROES,
-
-      id: 1,
-      name: 'Windstorm'
+      heroes: INITIAL_HEROES
     }
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(e) {
-    this.setState({ name: e.target.value });
+    const newName = e.target.value;
+    const selectedIdx = this.state.selectedIdx;
+    this.setState({
+      heroes: this.state.heroes.map((hero, idx) => {
+        return selectedIdx === idx ?
+          Object.assign(hero, { name: newName }) :
+          hero
+      })
+    });
   }
-  onSelect(hero) {
-    // console.log('asdfas')
-    // console.log(hero)
-    this.setState({ selectedIdx: hero.id });
+  onSelect(idx) {
+    this.setState({ selectedIdx: idx });
   }
   render() {
-    const selectedHero = this.state.heroes[this.state.selectedIdx]
+    const selectedIdx = this.state.selectedIdx;
+    const selectedHero = this.state.heroes[selectedIdx];
 
     return (
       <div>
         <h2>My Heroes</h2>
-        <ul class="heroes">
-          {this.state.heroes.map(hero =>
-            <li onClick={() => this.onSelect(hero)}>
-              <span class="badge">{hero.id}</span> {hero.name}
+        <ul className="heroes">
+          {this.state.heroes.map((hero, idx) =>
+            <li key={idx}
+            onClick={() => this.onSelect(idx)}
+            className={ idx === selectedIdx && "selected" }>
+              <span className="badge">{hero.id}</span> {hero.name}
             </li>
           )}
         </ul>
-        {<div>
-          <h2>{selectedHero.name.toUpperCase()} Details</h2>
-          <div><span>id: </span>{selectedHero.id}</div>
+        {this.state.selectedIdx !== null &&
           <div>
-            <label>name:
+            <h2>{selectedHero.name.toUpperCase()} Details</h2>
+            <div><span>id: </span>{selectedHero.id}</div>
+            <div>
+              <label>name:
               <input onChange={this.handleChange} value={selectedHero.name} placeholder={selectedHero.name} />
-            </label>
-          </div>
-        </div>}
-      </div>
-    )
+              </label>
+            </div>
+          </div>}
+      </div>)
   }
 }
