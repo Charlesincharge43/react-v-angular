61e3b8b79295996c3394005876dab563d14f67b5
Charles <charles.m.long43@gmail.com>
2018-07-13 22:07:11 -0500

    react almost done

--- react/src/Heroes/Heroes.js
+++ react/src/Heroes/Heroes.js
@@ -1,29 +1,50 @@
 import React, { Component } from 'react';
+import './Heroes.css';
 
-import { HEROES } from '../mock-heroes';
+import { INITIAL_HEROES } from '../mock-heroes';
 
 export default class Heroes extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
+      selectedIdx: null,
+      heroes: INITIAL_HEROES,
+
       id: 1,
       name: 'Windstorm'
     }
     this.handleChange = this.handleChange.bind(this);
   }
-  handleChange(e){
-    this.setState({name: e.target.value})
+  handleChange(e) {
+    this.setState({ name: e.target.value });
+  }
+  onSelect(hero) {
+    // console.log('asdfas')
+    // console.log(hero)
+    this.setState({ selectedIdx: hero.id });
   }
   render() {
+    const selectedHero = this.state.heroes[this.state.selectedIdx]
+
     return (
       <div>
-        <h2>{this.state.name.toUpperCase()} Details</h2>
-        <div><span>id: </span>{ this.state.id }</div>
-        <div>
-          <label>name:
-            <input onChange={this.handleChange} value={this.state.name} placeholder={this.state.name} />
-          </label>
-        </div>
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+          {this.state.heroes.map(hero =>
+            <li onClick={() => this.onSelect(hero)}>
+              <span class="badge">{hero.id}</span> {hero.name}
+            </li>
+          )}
+        </ul>
+        {<div>
+          <h2>{selectedHero.name.toUpperCase()} Details</h2>
+          <div><span>id: </span>{selectedHero.id}</div>
+          <div>
+            <label>name:
+              <input onChange={this.handleChange} value={selectedHero.name} placeholder={selectedHero.name} />
+            </label>
+          </div>
+        </div>}
       </div>
     )
   }
--- react/src/mock-heroes.js
+++ react/src/mock-heroes.js
@@ -1,4 +1,4 @@
-export const HEROES = [
+export const INITIAL_HEROES = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
