6dc2b3244ba394a008a68fedad88afae0af618d3
Charles <charles.m.long43@gmail.com>
2018-07-18 22:12:23 -0500

    React: Heroes class properties update

--- react/src/Heroes/Heroes.js
+++ react/src/Heroes/Heroes.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 
 export default class Heroes extends Component {
-  constructor(){
-    super()
-    this.state = {
-      id: 1,
-      name: 'Windstorm'
-    }
-    this.handleChange = this.handleChange.bind(this);
+  state = {
+    id: 1,
+    name: 'Windstorm'
   }
-  handleChange(e){
-    this.setState({name: e.target.value})
-  }
+
+  handleChange = (e) => this.setState({ name: e.target.value })
+  
   render() {
     return (
       <div>
         <h2>{this.state.name.toUpperCase()} Details</h2>
-        <div><span>id: </span>{ this.state.id }</div>
+        <div><span>id: </span>{this.state.id}</div>
         <div>
           <label>name:
             <input onChange={this.handleChange} value={this.state.name} placeholder={this.state.name} />
